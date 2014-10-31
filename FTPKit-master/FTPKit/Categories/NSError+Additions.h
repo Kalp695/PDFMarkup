bd6d45bb91a39708c0de6942398bca68ca0f0204
ravi <istore@ravi-Mac-mini.local>
2014-10-31 22:31:12 +0530

    Commit

---
+++ FTPKit-master/FTPKit/Categories/NSError+Additions.h
@@ -0,0 +1,14 @@
+
+extern NSString *const FTPErrorDomain;
+
+@interface NSError (NSError_FTPKitAdditions)
+
+/**
+ Returns an error for the respective FTP error code.
+ 
+ @param errorCode FTP error code
+ @return NSError Respective message for error code.
+ */
++ (NSError *)FTPKitErrorWithCode:(int)errorCode;
+
+@end
---
+++ FTPKit-master/FTPKit/Categories/NSString+Additions.h
@@ -0,0 +1,23 @@
+
+@interface NSString (NSString_FTPKitAdditions)
+
+/**
+ URL encode a string.
+ 
+ @param string String to URL encode
+ @return NSString Encoded URL string
+ */
++ (NSString *)FTPKitURLEncodeString:(NSString *)string;
+
+/**
+ URL decode a string.
+ 
+ @param string String to URL decode
+ @return NSString Decoded URL string.
+ */
++ (NSString *)FTPKitURLDecodeString:(NSString *)string;
+
+- (NSString *)FTPKitURLEncodedString;
+- (NSString *)FTPKitURLDecodedString;
+
+@end
